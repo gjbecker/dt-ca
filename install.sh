33fecea79154ca9ea0dda123d25340e1d5aa9ea5
gjbecker <gjbecker@iastate.edu>
2024-01-17 14:48:13 -0600

    update install

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+
+MAKE_VENV=${1:-true}
+SOURCE_VENV=${2:-true}
+
+# Directory of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+GYM_DIR="$DIR/decision_transformer/envs/gym_ca_data_gen"
+
+if $MAKE_VENV; then
+    # Virtualenv w/ python3
+    export PYTHONPATH=/usr/bin/python3 # point to your python3
+    python3 -m pip install virtualenv
+    cd $DIR
+    python3 -m virtualenv venv
+fi
+
+if $SOURCE_VENV; then
+    cd $DIR
+    source venv/bin/activate
+    export PYTHONPATH=${DIR}/venv/bin/python/dist-packages
+fi
+
+# Install this pkg and its requirements
+python -m pip install -e $GYM_DIR
+
+# Install RVO and its requirements
+cd $GYM_DIR/gym_collision_avoidance/envs/policies/Python-RVO2
+python -m pip install Cython
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    export MACOSX_DEPLOYMENT_TARGET=10.15
+    brew install cmake || true
+fi
+python setup.py build
+python setup.py install
+
+echo "Finished installing gym_collision_avoidance!"
+
+cd $DIR
+python -m pip install -r requirements.txt
+
+echo "Finished installing dt_ca!"
